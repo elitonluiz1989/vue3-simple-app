77ef4c5682e8c8d0e5dda32758827c4d961c33ef
Eliton Luiz <eliton1989@gmail.com>
2020-10-18 20:17:59 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue 3 Simple App
+
+A simple webpage with three demo pages.
+
+Made with:
+- Vue 3
+- Vue Router
+- Vuex
+- Typescript
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header container">
+    <div class="row justify-content-center">
+      <div class="col-4">
+        <div class="header-logo">
+          <img
+            :src="logoSettings.src"
+            :alt="logoSettings.src"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+
+      <div class="col"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    const logoSettings = {
+      src: "",
+      alt: "",
+    };
+
+    export { logoSettings };
+  },
+};
+</script>
+
+<style>
+</style>
